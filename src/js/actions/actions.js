af6d54f4da8f1afcfd42b15ed2dcc1b871adc3fd
GeordieP <geordie.powers@gmail.com>
2017-11-08 19:23:18 -0500

    Added some confirmation steps and error alerts

--- src/js/actions/actions.js
+++ src/js/actions/actions.js
@@ -2,21 +2,35 @@ export default {
     toggleEdit: ({ editMode }) => ({ editMode: !editMode }),
 
     savePage: ({ pages }, _, { key, newItem, page }) => {
+        if (!page.url) {
+            alert("Save failed: No URL provided")
+            return
+        }
+
+        if (!page.name) {
+            if (!confirm("No page name given, continue with default 'NO_NAME'?"))
+                return
+
+            page.name = "NO_NAME"
+        }
+
+        // if this is a new page, create a key
         if (newItem) { key = Date.now() }
+
         let newPages = Object.assign({}, pages)
         // update or replace object at key
         newPages[key] = page
         return ({ pages: newPages })
     },
+
     deletePage: ({ pages }, _, key) => {
-        // if page key exists, delete it
-        if (pages[key]) {
+        if (!pages[key]) {
+            alert("Something went wrong. No page exists with the given key.")
+        } else if (confirm("Delete " + pages[key].name + "? There is no undo.")) {
             let newPages = Object.assign({}, pages)
             delete newPages[key]
             return { pages: newPages }
         }
-
-        return { pages }
     }
 }
 
--- src/js/components/EditForm.js
+++ src/js/components/EditForm.js
@@ -9,17 +9,17 @@ export default ({ key, page, newItem, savePage, deletePage }) => {
     let inputUrl = key + "_url"
     let inputColor = key + "_color"
 
-    let onSubmit = function({ originalTarget }) {
+    let onSubmit = function({ target }) {
         savePage({
             key,
             // undefined is falsy, but give the action a boolean for more predictable behavior
             newItem: newItem || false, 
             page: {
                 // default name "NO NAME"
-                name: originalTarget[inputName].value || "NO NAME",
-                url: originalTarget[inputUrl].value,
+                name: target[inputName].value,
+                url: target[inputUrl].value,
                 // default color 1f1f1f
-                color: originalTarget[inputColor].value || "#1f1f1f"
+                color: target[inputColor].value || "#1f1f1f"
             }
         })
     }
